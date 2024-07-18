36c2de6091eda387cdae6e779eb9c7a630374d96
yuxnzs <yuchengliu0429@gmail.com>
2024-07-19 02:00:59 +0800

    Update JSON structure to include first week ranking from billboard history

--- MusicRankTracker/Models/BillboardHistoryModel.swift
+++ MusicRankTracker/Models/BillboardHistoryModel.swift
@@ -14,12 +14,13 @@ struct BillboardHistoryData: Codable, Identifiable {
     var id = UUID()
     let song: String
     let artist: String
+    let first_week: Int
     let this_week: Int
     let last_week: Int?
     let peak_position: Int
     let weeks_on_chart: Int
     
     enum CodingKeys: String, CodingKey {
-        case song, artist, this_week, last_week, peak_position, weeks_on_chart
+        case song, artist, first_week, this_week, last_week, peak_position, weeks_on_chart
     }
 }
