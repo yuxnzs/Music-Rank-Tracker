91abf07f6d46078b4032f81108423ff6cfc76eee
yuxnzs <yuchengliu0429@gmail.com>
2025-12-18 20:27:31 +0800

    Add isDateView mode to LoadingBillboardHistoryView with rank number display

--- MusicRankTracker/Views/LoadingBillboardHistoryView.swift
+++ MusicRankTracker/Views/LoadingBillboardHistoryView.swift
@@ -2,15 +2,37 @@ import SwiftUI
 
 struct LoadingBillboardHistoryView: View {
     let bottomSafeArea: CGFloat
+    let isDateView: Bool
+    
+    init(bottomSafeArea: CGFloat, isDateView: Bool = false) {
+        self.bottomSafeArea = bottomSafeArea
+        self.isDateView = isDateView
+    }
     
     var body: some View {
-        ArtistInfo(isPlaceholder: true, shorterTotalCountPlaceholder: true)
-            .padding(.bottom, 20)
+        if !isDateView {
+            ArtistInfo(isPlaceholder: true, shorterTotalCountPlaceholder: true)
+                .padding(.bottom, 20)
+        }
         
         VStack {
             ForEach(0..<10, id: \.self) { index in
-                RankInfo(isPlaceholder: true)
+                if isDateView {
+                    HStack(spacing: 0) {
+                        Text("\(index + 1)")
+                            .frame(width: 25, alignment: .center)
+                            .font(.system(size: 18, weight: .bold))
+                            .padding(.horizontal)
+                        
+                        RankInfo(isPlaceholder: true)
+                            .padding(.trailing)
+                    }
                     .padding(.bottom, 20)
+                } else {
+                    RankInfo(isPlaceholder: true)
+                        .padding(.horizontal)
+                        .padding(.bottom, 20)
+                }
             }
         }
         .padding(.bottom, bottomSafeArea + 20)
@@ -18,5 +40,5 @@ struct LoadingBillboardHistoryView: View {
 }
 
 #Preview {
-    LoadingBillboardHistoryView(bottomSafeArea: 34)
+    LoadingBillboardHistoryView(bottomSafeArea: 34, isDateView: true)
 }
